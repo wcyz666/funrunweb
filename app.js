51d99e093228b04baf8283fbcc0341e6bac44738
Wang Cheng <wcyz666@126.com>
2015-04-30 17:58:05 +0800

    Enhancement:
    	1. Add init page

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var bodyParser = require('body-parser');
 var utils = require('./my_modules/utils');
 var path = require('path');
 var handlebars = require('express-handlebars').create({});
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
+var mysql = require('mysql');
 
 var roomList = {};
 
-var mysql      = require('mysql');
+connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : "FUNRUNWEB"
+});
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
@@ -23,6 +29,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 app.use( express.static( __dirname + '/public' ) );
 
+
+
 app.get('/', function(req, res){
     var connection;
 
@@ -30,16 +38,23 @@ app.get('/', function(req, res){
         res.cookie("id", "helloworld",{maxAge: 1000 * 86400});
     }
     res.render("login", {state: false});
-    connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : "FUNRUNWEB"
-    });
     connection.connect();
     connection.query('SELECT ', function(err, rows, fields) {
         //console.log('The solution is: ', rows[0].solution);
     });
+    connection.end();
+});
+
+/* GET users listing. */
+app.get('/init', function(req, res) {
+    connection.connect();
+    connection.query('CREATE DATABASE funrunweb', function(err, rows, fields) {
+    });
+    connection.query('CREATE TABLE user (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, username CHAR(20) NOT NULL,' +
+    'password CHAR(40) NOT NULL, score INTEGER)', function(err, rows, fields) {
+        if (err) throw err;
+    });
+    
     connection.end();
 });
 
